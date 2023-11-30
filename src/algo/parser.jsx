ee58425a88db3a733d37118747d6632d55ea3078
Henry Anand <39207406+henryanandsr@users.noreply.github.com>
2023-11-30 20:24:47 +0700

    fix : parsing

--- src/algo/parser.jsx
+++ src/algo/parser.jsx
@@ -1,27 +1,15 @@
 export default function Parser(sequence) {
     var operations = sequence.split(';').map(operation => operation.trim());
-
-    var transactions = {};
     var result = [];
-
+  
     operations.forEach(operation => {
-        const [type, transaction, resource] = operation.match(/([RW])(\d+)\((\w+)\)/) || [];
-        if (type && transaction && resource) {
-            if (!transactions[transaction]) {
-                transactions[transaction] = { readSet: new Set(), writeSet: new Set() };
-            }
-
-            if (type === 'R') {
-                transactions[transaction].readSet.add(resource);
-            } else if (type === 'W') {
-                transactions[transaction].writeSet.add(resource);
-            }
-
-            result.push({ type, transaction, resource });
-        } else if (operation.startsWith('C')) {
-            result.push({ type: 'C' + operation.substr(1), transaction: 'C', resource: operation.substr(1) });
-        }
+      const matchResult = operation.match(/([RW])(\w+)\((\w+)\)/);
+      const [type, transaction, resource] = matchResult || [];
+      if (type && transaction && resource) {
+        result.push({ type, transaction, resource });
+      } else if (operation.startsWith('C')) {
+        result.push({ type: 'C', transaction: 'C', resource: operation.substr(1) });
+      }
     });
-
     return result;
-}
+  }  
